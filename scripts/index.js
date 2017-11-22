d75de5481953bf3215745e5fad4d3f42ba960d04
Isaac Orme <isorme1@gmail.com>
2017-11-22 15:13:41 -0800

    implemented-tie-game-double-stakes-functionality

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,22 @@
 let player1Roll = document.querySelector('#p1-roll');
 let player2Roll = document.querySelector('#p2-roll');
 
+// adding players related elements
 let players =  document.querySelector('.players')
 let addPlayerBtns = players.querySelectorAll('button');
 
-
-// probably removing these two and implementing a 2-5 player kind of thing
 // player 1 and 2 dice selection
 let player1Dice = document.querySelector('.p1-dice-select');
 let player2Dice = document.querySelector('.p2-dice-select');
 
-
 // all players dice selections
+// haven't implemented full functionality for more than 2 players yet
 let allPlayersDice = document.querySelector('.dice-select');
 let allPlayersDiceHolders = allPlayersDice.querySelectorAll('div');
 
 //scoreboard elements additional
 let scoreBoard = document.querySelector('.scoreboard');
 let scoreBoardPlayers = scoreBoard.querySelectorAll('td');
-
 
 // stakes related elements
 let stakes = document.querySelector('.stakes');
@@ -34,15 +32,17 @@ let p2Scoreboard = document.getElementById('p2-score');
 let currentRoll = document.querySelectorAll('.c-roll');
 let audio = document.getElementById('sound');
 
-// players' score holder variables (starts at -5 due to Dice Scoring Rules in this game)
+// players' score holder variables (starts at -5 to cancel out adding the qualifying dice rolls of 1 and 4)
 let totalForp1 = -5;
 let totalForp2 = -5;
 
-let counter = 0;
+let counter = 0;  // keeps track of how many times a die has been chosen by a player during their turn
 let firstRoll = true;
 let preventRoll;
-let preventSound;
+let preventSound; // prevents dice sound from playing when clicking roll button if they havent chosen a die yet
 
+
+// this function runs based off an "onclick" in the roll buttons that are located inside the "roll-buttons div"
 function roll() {
   if(preventRoll) {
     swal('You must choose at least one die before rolling again');
@@ -66,6 +66,12 @@ function roll() {
     totalForp1 = -5;
     totalForp2 = -5;
     firstRoll = true;
+      // checks to see if this was NOT a tie break game
+    if (  (Number(currentPot.innerHTML)) === (Number(stakeCurrent.innerHTML) * numOfPlayers) ) {
+      p1Scoreboard.innerHTML = ( Number(p1Scoreboard.innerHTML) - Number(stakeCurrent.innerHTML) );
+      p2Scoreboard.innerHTML = ( Number(p2Scoreboard.innerHTML) - Number(stakeCurrent.innerHTML) );
+    }
+
   }
 
   // if its the first roll, we do a standard loop and add random numbers to current roll
@@ -77,8 +83,8 @@ function roll() {
     firstRoll = false;
     return;
   }
-  // if it is NOT the first roll, some elements of current roll will be missing and we need
-  // to make sure they do not get filled in with random numbers
+  // if it is NOT the first roll, some elements of current roll will be removed and we need
+  // to make sure they do not get filled in with random numbers so they stay removed
   if (firstRoll === false) {
     for (var i = 0; i < 6; i++) {
       document.getElementById(`${i}`).innerHTML === "" ? "" : document.getElementById(`${i}`).innerHTML = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
@@ -88,11 +94,7 @@ function roll() {
 }
 
 function scoreTotalerP1() {
-
-
-
-
-  let p1Qualify = [];
+  let p1Qualify = [];  // tests to see if dice selection includes 1 and 4 to qualify
   let player1Scores = Array.from(player1Dice.querySelectorAll('span'));
   player1Scores.forEach(function(score){
     if ( (score.classList.contains('1') || score.classList.contains('4')) && p1Qualify.length < 2 ) {
@@ -123,31 +125,39 @@ function scoreTotalerP2() {
 
 
 function scoreboardHandler() {
+  // giving a player a score of 0 is used only to indicate they have not qualified
   if (player1Dice.innerHTML === "YOU DON'T QUALIFY!") {
     totalForp1 = 0;
   }
   if (player2Dice.innerHTML === "YOU DON'T QUALIFY!") {
     totalForp2 = 0;
+
   }
 
-  // work on tie section, below. currently it just goes into a new round with any1 winning.
-  // which is ok, but we need to double the stakes when its a tie....
-  // also, think abnbout using current pot to add to players scoreboard score instead of stakeCurrent
-
+  if (totalForp2 === 0 && totalForp1 === 0) {
+    //both players did not qualify
+    swal('No winners here. All players ante up!');
+    return;
+  }
 
   if ( (totalForp1 !== -5 && totalForp1 !== 0) && (totalForp1 === totalForp2) ) {
-    swal("It's a Tie!")
+    swal("It's a Tie! Ante up!");
+    p1Scoreboard.innerHTML = (Number(p1Scoreboard.innerHTML) - Number(stakeCurrent.innerHTML));
+    p2Scoreboard.innerHTML = (Number(p2Scoreboard.innerHTML) - Number(stakeCurrent.innerHTML));
+    currentPot.innerHTML = Number(currentPot.innerHTML) + (Number(numOfPlayers) * Number(stakeCurrent.innerHTML));
+
     return;
   }
   if (totalForp1 !== -5 && totalForp2 !== -5 && stakeCurrent.innerHTML !== "") {
-    totalForp1 > totalForp2 ? p1Scoreboard.innerHTML = (Number(p1Scoreboard.innerHTML) + Number(stakeCurrent.innerHTML)) : p2Scoreboard.innerHTML = (Number(p2Scoreboard.innerHTML) + Number(stakeCurrent.innerHTML));
-    totalForp1 > totalForp2 ? swal("Player 1 Wins the monies!") : swal("Player 2 Wins the monies!");
+    totalForp1 > totalForp2 ? p1Scoreboard.innerHTML = (Number(p1Scoreboard.innerHTML) + Number(currentPot.innerHTML)) : p2Scoreboard.innerHTML = (Number(p2Scoreboard.innerHTML) + Number(currentPot.innerHTML));
+    totalForp1 > totalForp2 ? swal("Player 1 Wins!") : swal("Player 2 Wins!");
+    currentPot.innerHTML = Number(stakeCurrent.innerHTML) * numOfPlayers;  // reset pot to standard size in case it was a tie break game with a larger than normal current pot
     return;
   }
 }
 
-
-function chosen(num,i) {
+// this function runs everytime a player chooses a die from the current roll
+function chosen() {
   preventRoll = false;
   counter += 1;
   // When counter is 6, player has chosen last die and their turn is over
@@ -155,12 +165,7 @@ function chosen(num,i) {
   // Player 2's turn is over
   if (player1Roll.classList.contains('hide') && counter === 6) {
     counter = 0;
-
-
-    // here  !!!!
-    player2Dice.innerHTML +=  `<span style="font-size: 6rem; color:black;" class="${this.dataset.value}">${this.innerHTML}</span>`;
-
-
+    player2Dice.innerHTML +=  `<span style="font-size: 6rem; color:red;" class="${this.dataset.value}">${this.innerHTML}</span>`;
     this.innerHTML = "";
     player1Roll.classList.remove("hide");  // show player 1's roll button
     player2Roll.className += " hide";  // hide player 2's roll button
@@ -171,12 +176,7 @@ function chosen(num,i) {
   } else if (player2Roll.classList.contains('hide') && counter === 6) {
     counter = 0;
 
-
-    // here !!!!
-    player1Dice.innerHTML +=  `<span style="font-size: 6rem; color:black;" class="${this.dataset.value}">${this.innerHTML}</span>`;
-
-
-
+    player1Dice.innerHTML +=  `<span style="font-size: 6rem; color:red;" class="${this.dataset.value}">${this.innerHTML}</span>`;
     this.innerHTML = "";
     player2Roll.classList.remove("hide");
     player1Roll.className += " hide";
@@ -188,30 +188,23 @@ function chosen(num,i) {
    scoreboardHandler();
 
    // if counter is 0 that means one of the conditionals has executed so
-   // we must exit function before bottom lines of code gets executed
+   // we must exit function before bottom lines of code get executed
    if (counter === 0) {
      return;
     }
 
-
-
-
-    // here !!!
-  // adds the chosen dice number to the current player's dice selection
-  //player1Roll.classList.contains('hide') ? player2Dice.innerHTML +=  `<span>${this.innerHTML}</span>` : player1Dice.innerHTML +=  `<span>${this.innerHTML}</span>`;
-
-
   if (player1Roll.classList.contains('hide')) {
-    player2Dice.innerHTML +=  `<span style="font-size: 6rem; color:black;" class="${this.dataset.value}">${this.innerHTML}</span>`;
+    player2Dice.innerHTML +=  `<span style="font-size: 6rem; color:red;" class="${this.dataset.value}">${this.innerHTML}</span>`;
   }
   if (player2Roll.classList.contains('hide')) {
-    player1Dice.innerHTML +=  `<span style="font-size: 6rem; color:black;" class="${this.dataset.value}">${this.innerHTML}</span>`;
+    player1Dice.innerHTML +=  `<span style="font-size: 6rem; color:red;" class="${this.dataset.value}">${this.innerHTML}</span>`;
   }
 
   // removes the chosen dice number from current roll
   this.innerHTML = "";
 }
 
+// this function runs when the roll button is clicked
 function playSoundAndDiceAnimation() {
   if (preventSound === false) {
     audio.currentTime = 0;
@@ -223,7 +216,6 @@ function playSoundAndDiceAnimation() {
 
       for (var i = 0; i < 6; i++) {
         faceValue = Number(document.getElementById(`${i}`).dataset.value) - 1;
-        console.log(faceValue);
         if (faceValue !== "" && faceValue !== -1) {
           output = "&#x268" + faceValue + "; ";
           document.getElementById(`${i}`).innerHTML = output;
@@ -238,9 +230,16 @@ function playSoundAndDiceAnimation() {
 function currentStakes() {
   stakeCurrent.innerHTML = this.innerHTML[1];
   currentPot.innerHTML = this.innerHTML[1] * 2;
+  p1Scoreboard.innerHTML = ( Number(p1Scoreboard.innerHTML) - Number(stakeCurrent.innerHTML) );
+  p2Scoreboard.innerHTML = ( Number(p2Scoreboard.innerHTML) - Number(stakeCurrent.innerHTML) );
 }
 
 function addPlayers() {
+  if (stakeCurrent.innerHTML === "") {
+    swal("Choose your stakes first");
+    return;
+  }
+  numOfPlayers = this.innerHTML;
   diceHolderArray = Array.from(allPlayersDiceHolders);
 
   for (var i = 0; i < this.innerHTML; i++) {
